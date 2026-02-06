c028b45825fc17d417e294c2db37726b86edd82c
Michal <michalnuszkiewiczbusiness@gmail.com>
2026-02-06 18:50:29 +0100

    Created docker-compose for Kafka, Updated Config

---
+++ src/main/java/com/micnusz/edns/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.micnusz.edns.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic eventsTopic() {
+        return TopicBuilder.name("events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
--- src/main/java/com/micnusz/edns/service/EventService.java
+++ src/main/java/com/micnusz/edns/service/EventService.java
@@ -1,7 +1,14 @@
 package com.micnusz.edns.service;
 
 
+import com.micnusz.edns.dto.EventRequest;
+import com.micnusz.edns.dto.EventResponse;
+import com.micnusz.edns.mapper.EventMapper;
+import com.micnusz.edns.model.Event;
+import com.micnusz.edns.model.User;
 import com.micnusz.edns.repository.EventRepository;
+import com.micnusz.edns.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +17,19 @@ import org.springframework.stereotype.Service;
 public class EventService {
 
     private final EventRepository eventRepository;
+    private final UserRepository userRepository;
+    private final EventMapper eventMapper;
+
+    public EventResponse createEvent(EventRequest request) {
+
+        User recipient = userRepository.findById(request.getRecipientId())
+                .orElseThrow(() -> new EntityNotFoundException("Recipient not found"));
+
+        User actor = userRepository.findById(request.getActorId())
+                .orElseThrow(() -> new EntityNotFoundException("Actor not found"));
+
+        Event event = eventMapper.toEntity(request, recipient, actor);
+        Event saved = eventRepository.save(event);
+        return eventMapper.toDto(saved);
+    }
 }
